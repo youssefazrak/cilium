a1810ded58cb13af92a3d250a483e9f763e7de21
Daniel Borkmann <daniel@iogearbox.net>
2020-09-01 08:30:25 +0200

    bpf: migrate tail call maps to avoid altering currently loaded ones
    We currently only migrate maps upon property mismatch when we reload BPF
    programs, but we also need to do this for tail call maps. The currently
    loaded ones should not be altered as this results in weird behavior until
    the main prog has been replaced. Thus even if there was no property
    mismatch, move the pinned tail call map to a different location and then
    upon success unlink it. Remove the 'unlinking migrated node' message
    since it is not worthy to warn here.
    
    Fixes: #13015
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

---
+++ bpf/lib/ids.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/* Copyright (C) 2016-2020 Authors of Cilium */
+
+#ifndef __LIB_IDS_H_
+#define __LIB_IDS_H_
+
+#define CILIUM_MAP_POLICY	1
+#define CILIUM_MAP_CALLS	2
+
+#endif /* __LIB_IDS_H_ */
