youssefazrak/cilium

commit b194612c004c3e69289286e9a35d337b2645fc50

identity: Avoid kvstore lookup for local identities
Reserved and CIDR identities are local to the agent and not stored in
the kvstore. This commit changes the identity cache to avoid performing
a kvstore lookup for CIDR entries (which is currently done when a CIDR
identity is released).

This is a follow-up to
https://github.com/cilium/cilium/pull/13205#discussion_r490306171

Signed-off-by: Sebastian Wicki <sebastian@isovalent.com>

pkg/identity/cache/cache.go
@@ -211,7 +211,7 @@ func (m *CachingIdentityAllocator) LookupIdentity(ctx context.Context, lbls labe
 		return identity
 	}
 
-	if m.IdentityAllocator == nil {
+	if !identity.RequiresGlobalIdentity(lbls) || m.IdentityAllocator == nil {
 		return nil
 	}
 
@@ -242,12 +242,12 @@ func (m *CachingIdentityAllocator) LookupIdentityByID(ctx context.Context, id id
 		return identity
 	}
 
-	if m.IdentityAllocator == nil {
-		return nil
-	}
-
 	if identity := m.localIdentities.lookupByID(id); identity != nil {
 		return identity
+	}
+
+	if id.HasLocalScope() || m.IdentityAllocator == nil {
+		return nil
 	}
 
 	allocatorKey, err := m.IdentityAllocator.GetByIDIncludeRemoteCaches(ctx, idpool.ID(id))

commit 3cb7f88a63ebdc3255d8c4b474aeb6317375f02a

identity: Fix nil pointer panic in LookupIdentityByID
Because the identity allocator is initialized asychronously via
`InitIdentityAllocator`, the local identitiy allocator might not have
been initialized yet when the lookup functions are called. This can
cause nil pointer panics, as observed in #13479.

Before b194612c004c3e69289286e9a35d337b2645fc50, this nil pointer panic
could not occur in `LookupIdentityByID` as the function checked for
`m.IdentityAllocator != nil` which also implies `m.localIdentities != nil`.
This commit adds an explict check for `m.localIdentities`.

Fixes: #13479
Fixes: b194612c004c ("identity: Avoid kvstore lookup for local identities")

Signed-off-by: Sebastian Wicki <sebastian@isovalent.com>

pkg/identity/cache/cache.go
@@ -207,8 +207,11 @@ func (m *CachingIdentityAllocator) LookupIdentity(ctx context.Context, lbls labe
 		return reservedIdentity
 	}
 
-	if identity := m.localIdentities.lookup(lbls); identity != nil {
-		return identity
+	// m.localIdentities can be nil if InitIdentityAllocator has not yet been completed
+	if m.localIdentities != nil {
+		if identity := m.localIdentities.lookup(lbls); identity != nil {
+			return identity
+		}
 	}
 
 	if !identity.RequiresGlobalIdentity(lbls) || m.IdentityAllocator == nil {
@@ -242,8 +245,11 @@ func (m *CachingIdentityAllocator) LookupIdentityByID(ctx context.Context, id id
 		return identity
 	}
 
-	if identity := m.localIdentities.lookupByID(id); identity != nil {
-		return identity
+	// m.localIdentities can be nil if InitIdentityAllocator has not yet been completed
+	if m.localIdentities != nil {
+		if identity := m.localIdentities.lookupByID(id); identity != nil {
+			return identity
+		}
 	}
 
 	if id.HasLocalScope() || m.IdentityAllocator == nil {